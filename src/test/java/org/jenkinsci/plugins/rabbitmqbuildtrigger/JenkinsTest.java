563721a0cd7c4c37f31c30397a7fb844be89cc88
rinrinne <rinrin.ne@gmail.com>
2014-08-19 15:04:02 +0900

    Add test for RemoteBuildPublisher

--- src/test/java/org/jenkinsci/plugins/rabbitmqbuildtrigger/JenkinsTest.java
+++ src/test/java/org/jenkinsci/plugins/rabbitmqbuildtrigger/JenkinsTest.java
@@ -1,5 +1,8 @@
 package org.jenkinsci.plugins.rabbitmqbuildtrigger;
 
+import java.util.concurrent.Future;
+
+import jenkins.model.Jenkins;
 import hudson.model.Build;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
@@ -9,18 +12,35 @@ import hudson.model.Project;
 import hudson.tasks.Shell;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.Verifications;
 
 import org.apache.commons.io.FileUtils;
 import org.jenkinsci.plugins.rabbitmqconsumer.extensions.MessageQueueListener;
+import org.jenkinsci.plugins.rabbitmqconsumer.publishers.PublishChannel;
+import org.jenkinsci.plugins.rabbitmqconsumer.publishers.PublishChannelFactory;
+import org.jenkinsci.plugins.rabbitmqconsumer.publishers.PublishResult;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
 
+import com.rabbitmq.client.AMQP;
+
 public class JenkinsTest {
     // CS IGNORE VisibilityModifier FOR NEXT 3 LINES. REASON: Mocks tests.
     @Rule
     public JenkinsRule j = new JenkinsRule();
+
+    @Mocked
+    PublishChannelFactory factory = null;
+
+    @Mocked
+    PublishChannel channel = null;
+
+    @Mocked
+    Future<PublishResult> future = null;
 
     @Test
     public void testTriggerBuild() throws Exception {
@@ -56,6 +76,52 @@ public class JenkinsTest {
         try {Thread.sleep(3000);} catch (Exception e) {}
 
         assertThat(project.getBuilds().isEmpty(), is(true));
+    }
+
+    @Test
+    public void testTriggerWithPublisher() throws Exception {
+        new NonStrictExpectations() {{
+            PublishChannelFactory.getPublishChannel(); result = channel;
+            channel.isOpen(); result = true;
+            channel.publish(anyString, anyString, (AMQP.BasicProperties)any, new byte[]{anyByte}); result = future;
+            future.get(); result = new PublishResult(true, "", "");
+        }};
+        RemoteBuildTrigger trigger = new RemoteBuildTrigger("trigger-token");
+        RemoteBuildPublisher publisher = new RemoteBuildPublisher("exchange", "routing-key");
+        FreeStyleProject project = j.createFreeStyleProject("triggered-project-publisher");
+        project.addTrigger(trigger);
+        project.getPublishersList().add(publisher);
+        project.getBuildersList().add(new Shell("echo TRIGGERED"));
+        trigger.start(project, false);
+
+        String msg = "{\"project\":\"triggered-project-publisher\",\"token\":\"trigger-token\"}";
+        RemoteBuildListener listener = MessageQueueListener.all().get(RemoteBuildListener.class);
+        listener.onReceive("trigger-queue", "application/json", null, msg.getBytes("UTF-8"));
+
+        waitForBuildCompleted(project);
+
+        FreeStyleBuild build = project.getBuilds().getFirstBuild();
+        String s = FileUtils.readFileToString(build.getLogFile());
+        assertThat(s, containsString("TRIGGERED"));
+
+        new Verifications() {{
+            String exchangeName;
+            String routingKey;
+            AMQP.BasicProperties props;
+            byte[] body;
+            channel.publish(
+                    exchangeName=withCapture(),
+                    routingKey=withCapture(),
+                    props=withCapture(),
+                    body=withCapture());
+            times = 1;
+
+            assertThat(exchangeName, is("exchange"));
+            assertThat(routingKey, is("routing-key"));
+            assertThat(props.getAppId(), is(RemoteBuildTrigger.PLUGIN_APPID));
+            assertThat(props.getContentType(), is("application/json"));
+            assertThat(props.getHeaders().get("jenkins-url").toString(), is(Jenkins.getInstance().getRootUrl()));
+        }};
     }
 
     @Test
